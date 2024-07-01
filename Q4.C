dd227a26b6feeda5349d2e350feebfddc4df8ab5
JAYDIPVORA7 <jaydipvora773@gmail.com>
2024-07-01 11:27:21 +0530

    Add files via upload

---
+++ Q4.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*
+1 0 1 0 1
+  1 0 1 0
+    1 0 1
+      1 0
+        1
+        
+*/
+main()
+{
+	int r,c,s;
+	
+	for(r=5;r>=1;r--)
+	{
+		for(s=5;s>r;s--)
+		{
+			printf("  ");
+		}
+		for(c=1;c<=r;c++)
+	    {
+			if(c%2==0)
+			{
+				printf("0 ");
+			}
+			else
+			{
+				printf("1 ");
+			}	
+	    }
+	    printf("\n");
+	}
+}
